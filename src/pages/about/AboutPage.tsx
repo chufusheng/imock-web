de7655c86034f50799c7e44edf7011e4df14d077
chufusheng <chufusheng3@163.com>
2021-01-14 15:54:39 +0800

    0114

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
@@ -7,10 +9,24 @@ import * as React from "react";
 import { Card, message } from "antd";
 import ReactMarkdown from "react-markdown"
 import myData from '../../source/README.md';
-import AgileTCEditor from 'react-agiletc-editor';
+import Kityminder from 'react-kityminder';
+import { useState } from "react";
+{/* <><script src="https://cdn.jsdelivr.net/npm/kity"></script><script src="https://cdn.jsdelivr.net/npm/kityminder-core"></script>
+    <script src="https://cdn.jsdelivr.net/npm/react-kityminder"></script></>
+// import AgileTCEditor from 'react-agiletc-editor'; */}
 
 
 const AboutPage = () => {
+    var json = { "template": "default", "root": { "data": { "created": 1562059643204, "id": "bv8nxhi3c800", "text": "测试" }, "children": [{ "data": { "created": 1609814203254, "id": "c8avibj2k880", "text": "用例" }, "children": [{ "data": { "resource": ["执行步骤"], "created": 1609814210520, "id": "c8aviev8fi00", "text": "步骤" }, "children": [{ "data": { "resource": ["执行步骤", "预期结果"], "created": 1609814216189, "id": "c8avihh01xs0", "text": "结果", "priority": 1 }, "children": [] }] }] }] }, "theme": "classic-compact", "version": "1.4.43", "base": 12 }
+    const [value, setValue] = useState(json)
+    const minderRef = React.useRef()
+    const minder = minderRef.current
+    const onChange = (value: any) => {
+        window.console.log('value',JSON.stringify(value))
+        window.console.log('mider',minder)
+        setValue(value)
+
+    }
 
     // const renderers = {
     //     code: () => {
@@ -23,10 +39,31 @@ const AboutPage = () => {
     // }, [props])
     return (
         <div>
-            <Card bordered title="About" style={{ margin: "16px 16px" }} >
+            {/* <Card bordered title="About" style={{ margin: "16px 16px" }} >
                 <ReactMarkdown source={myData} escapeHtml={false} />
-            </Card >
-            <AgileTCEditor
+            </Card > */}
+            <Kityminder
+                ref={minderRef}
+                value={value}
+                tags={['前置条件', '执行步骤', '预期结果']}
+                title="testjlksadjfkasdj"
+                onChange={onChange}
+                tabIndex='111'
+                progressShow={false}
+                readOnly={false}
+                mediaShow={!false}
+                // editorStyle={{ height: 'calc(100vh - 100px)' }}
+                toolbar={{
+                  image: false,
+                  theme: ['classic-compact', 'fresh-blue', 'fresh-green-compat'],
+                  template: ['default', 'right', 'fish-bone'],
+                }}
+                style={{
+                    width: '100%',
+                    height: '800px',
+                  }}
+            />
+            {/* <AgileTCEditor
                 // ref={editorNode => (this.editorNode = editorNode)}
                 tags={['前置条件', '执行步骤', '预期结果']}
                 progressShow={false}
@@ -49,7 +86,7 @@ const AboutPage = () => {
                         }
                         : null
                 }
-            />
+            /> */}
         </div>
     );
 }
